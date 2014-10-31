e39ee0ddc5027fd0bdc351651172d627ed4483b0
Alex Wallar <aw204@st-andrews.ac.uk>
2014-10-31 20:12:29 +0000

    stupid

--- roboserver/restful.py
+++ roboserver/restful.py
@@ -6,7 +6,7 @@ from flask import request, jsonify
 velocity_store = dict()
 
 
-@config.app("/vel/<name>", methods=["POST"])
+@config.app.route("/vel/<name>", methods=["POST"])
 def post_velocity(name):
     tilt_lr = request.form["tilt_lr"]
     tilt_fb = request.form["tilt_fb"]
@@ -14,6 +14,6 @@ def post_velocity(name):
     return jsonify(error=0, message="No bloody error")
 
 
-@config.app("/vel/<name>", methods=["GET"])
+@config.app.route("/vel/<name>", methods=["GET"])
 def get_velocity(name):
     pass
