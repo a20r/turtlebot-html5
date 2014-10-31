001cddda50ffffc8479b3f58ef8e7f2b37103128
Alex Wallar <aw204@st-andrews.ac.uk>
2014-10-31 20:24:40 +0000

    velocity

--- roboserver/config.py
+++ roboserver/config.py
@@ -3,3 +3,5 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+
+velocity_store = dict()
--- roboserver/restful.py
+++ roboserver/restful.py
@@ -3,17 +3,44 @@ import config
 from flask import request, jsonify
 
 
-velocity_store = dict()
+min_tilt = -30
+max_tilt = 30
 
 
 @config.app.route("/vel/<name>", methods=["POST"])
 def post_velocity(name):
-    tilt_lr = request.form["tilt_lr"]
-    tilt_fb = request.form["tilt_fb"]
-    print tilt_lr, tilt_fb
+    abs_tilt_lr = request.form["tilt_lr"]
+    abs_tilt_fb = request.form["tilt_fb"]
+    zero_lr = request.form["zero_lr"]
+    zero_fb = request.form["zero_fb"]
+
+    tilt_lr = abs_tilt_lr - zero_lr
+    tilt_fb = abs_tilt_fb - zero_fb
+
+    if tilt_lr > max_tilt:
+        tilt_lr = max_tilt
+
+    if tilt_lr < min_tilt:
+        tilt_lr = min_tilt
+
+    if tilt_fb > max_tilt:
+        tilt_fb = max_tilt
+
+    if tilt_fb < min_tilt:
+        tilt_fb = min_tilt
+
+    vel_x = tilt_fb / max_tilt
+    vel_y = tilt_lr / max_tilt
+
+    config.velocity_store[name] = (vel_x, vel_y)
+
     return jsonify(error=0, message="No bloody error")
 
 
 @config.app.route("/vel/<name>", methods=["GET"])
 def get_velocity(name):
-    pass
+    if name in config.velocity_store.keys():
+        vel = config.velocity_store[name]
+        return jsonify(vel_x=vel[0], vel_y=vel[1])
+    else:
+        return jsonify(vel_x=0, vel_y=0)
